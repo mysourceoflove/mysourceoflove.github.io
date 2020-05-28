244383dcbc3b69f7d2f3830d30d093e329182d3a
Frankluisravlo@gmail.com <Frankluisravlo@gmail.com>
2020-05-28 10:55:07 -0400

    Updates

---
+++ precache-manifest.b7b45da2a65500816225c44dd2704d3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4cb44a882b84eb00181ef08897e5431",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d85b762cd45f6bba6247",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "079f36176a59f6860af2",
+    "url": "/static/js/2.f763c6bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f763c6bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d85b762cd45f6bba6247",
+    "url": "/static/js/main.5ad57da0.chunk.js"
+  },
+  {
+    "revision": "8e2e666cf5cea7f2affa",
+    "url": "/static/js/runtime-main.0ec22328.js"
+  }
+]);
